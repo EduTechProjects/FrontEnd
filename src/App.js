d51d43b2fcd75a797e068e81108348703f704059
solji <oakikim@ewhain.net>
2023-05-28 10:13:12 +0900

    💄UI : subject.js, Home.js container 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -16,7 +17,11 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <Home />
+
         </a>
+        <div class="tenor-gif-embed" data-postid="23229777" data-share-method="host" data-aspect-ratio="1" data-width="100%"><a href="https://tenor.com/view/sound-gif-23229777">Sound GIF</a>from <a href="https://tenor.com/search/sound-gifs">Sound GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+        <iframe src="https://giphy.com/embed/aw6CWyyLQ8WyRuktxR" width="480" height="160" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/rainhouse-rainhouseco-sound-file-wav-aw6CWyyLQ8WyRuktxR">via GIPHY</a></p>
       </header>
     </div>
   );

31f3ffb170aa2fb711c7f9e3e2656e24b00da9ba
solji <oakikim@ewhain.net>
2023-06-02 20:47:46 +0900

    ✨feat : Router 추가

--- src/components/Feedback/AnswerContainer.js
+++ src/components/Feedback/AnswerContainer.js
@@ -2,11 +2,17 @@ import React from "react";
 import { Component } from "react";
 import styled from "styled-components";
 
+const Contaienr = styled.div`
+    width : 90%;
+
+`;
 
 const AnswerContainer = () =>{
 
     return (
         <Container>
+            <p></p>
+            <p></p>
             
         </Container>
 

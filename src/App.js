1d4b64305e22d529a39906c98fc74fa4e1d449ca
solji <oakikim@ewhain.net>
2023-06-05 14:33:13 +0900

    💄UI :  Feeback.js, Speak.js 완료, Subject.js 수정 중

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import Subject from "./pages/Subject.js";
 import Speak from "./pages/Speak";
 import Feedback from "./pages/Feedback";
@@ -9,18 +9,19 @@ import Navbar from "./components/Common/Navbar";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/subject" element={<Subject />} />
-          <Route path="/speak" element={<Speak />} />
-          <Route path="/feedback" element={<Feedback />} />
-        </Routes>
-        {/* <Subject /> */}
-      </div>
-    </BrowserRouter>
+     <BrowserRouter>
+        <Navbar />
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/subject" element={<Subject />} />
+            <Route path="/speak" element={<Speak />} />
+            <Route path="/feedback" element={<Feedback />} />
+          </Routes>
+           <subject /> 
+       </div>
+     </BrowserRouter>
+    
   );
 }
 
--- src/components/Feedback/AnswerContainer.js
+++ src/components/Feedback/AnswerContainer.js
@@ -1,18 +1,31 @@
 import React from "react";
-import { Component } from "react";
+import { ColorCode } from "./../../utils/Palette";
 import styled from "styled-components";
 
 const Container = styled.div`
-    width : 90%;
+    width : 70%;
+    background-color: white;
+    font-size :20px;
+    text-align: justify;
 
+`;
+
+const Document = styled.div`
+      padding-left:7vw;
+      text-align-last: left;
 `;
 
 const AnswerContainer = () =>{
 
     return (
         <Container>
-            <p></p>
-            <p></p>
+            <Document>
+            <p style={{color:"black", fontWeight:"bold"}}>Feedback</p>
+            <p style={{fontSize:"20px", color:"black"}}>
+            I can tell you about a popular and widely loved dish that many people enjoy, pizza. Pizza is a versatile and delicious food that originated in Italy but has become popular worldwide. It typically consists of a round, flat dough topped with tomato sauce, cheese, and various toppings such as vegetables, meats, and herbs. It is often baked in an oven until the crust is crispy and the cheese is melted and bubbly. Pizza offers a combination of flavors, textures, and toppings, making it a satisfying and enjoyable meal for many.
+            
+            </p>
+            </Document>
             
         </Container>
 

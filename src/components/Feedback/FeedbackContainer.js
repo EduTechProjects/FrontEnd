88e9e404bb2f199abf86c9c0043bd1c54ebc318c
LJH <049494@ewhain.net>
2023-06-17 17:41:50 +0900

    feat : FeedbackContainer props 부분 수정

--- src/components/Feedback/FeedbackContainer.js
+++ src/components/Feedback/FeedbackContainer.js
@@ -18,22 +18,11 @@ const Document = styled.div`
 const FeedbackContainer = ({ feedback }) => {
   return (
     <Container>
-      {/* 임시로 넣어두었습니담 */}
       <Document>
         <p style={{ color: ColorCode.SelectBlue, fontWeight: "bold" }}>
           Feedback
         </p>
-        {/* 더미데이터 만들어서 넣어보는 것 = 다음주  */}
-        <p style={{ fontSize: "20px", color: "black" }}>
-          I can tell you about a popular and widely loved dish that many people
-          enjoy, pizza. Pizza is a versatile and delicious food that originated
-          in Italy but has become popular worldwide. It typically consists of a
-          round, flat dough topped with tomato sauce, cheese, and various
-          toppings such as vegetables, meats, and herbs. It is often baked in an
-          oven until the crust is crispy and the cheese is melted and bubbly.
-          Pizza offers a combination of flavors, textures, and toppings, making
-          it a satisfying and enjoyable meal for many.
-        </p>
+        <p style={{ fontSize: "20px", color: "black" }}>{feedback}</p>
       </Document>
     </Container>
   );
